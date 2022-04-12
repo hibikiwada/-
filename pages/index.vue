de70b0d93f9ef07776cdb402b02e72029e3ef51a
hibikiwada <hibiki8479@gmail.com>
2022-04-12 19:09:46 +0900

    {JZ-684}/{3/25}/{イベントページ追加_SecurityDaysイベントレポート}/{テスト環境公開}

--- pages/index.vue
+++ pages/index.vue
@@ -4,9 +4,18 @@
       <section class="fv">
         <!-- <img src="/images/fv.jpg" alt="" class="fv-pc"> -->
         <!-- <img src="/images/sp-fv.png" alt="" class="fv-sp"> -->
-        <p>ささら 優<span>(ゆう)</span><br>
+        <p>
+          ささら 優<span>(ゆう)</span><br />
           <span class="sp2">〜 Woman creators Bank 所属イラストレーター 〜</span>
         </p>
+      </section>
+
+      <section class="renewal">
+        <p>ホームページをリニューアルしました。<br>
+          ↓の画像をクリックすると新しいホームページに遷移できます。</p>
+        <a href="http://yujump.lovepop.jp/" target="_blank">
+          <img src="/images/contact.jpg" alt="" />
+        </a>
       </section>
 
       <section class="profile">
@@ -17,23 +26,21 @@
           <div class="profile-l">
             <h3>
               ささら 優(ゆう)
-
             </h3>
             <p class="profile-l-txt">
-              <span>本名: </span>おがわ ゆうこ <br>
-              <span>現住所: </span>東京都 <br>
-              <span>趣味: </span>テニス、釣り <br>
-              <span>特徴: </span>1度あったら忘れない <br>
-              <span>使用Mac: </span>2.２GHz6 Intel Core i7 <br>
-              <span>使用ソフト: </span> <br>
-              CLIP STUDIO RAINT,
-              Adobe Illustrator 2020, <br>
+              <span>本名: </span>おがわ ゆうこ <br />
+              <span>現住所: </span>東京都 <br />
+              <span>趣味: </span>テニス、釣り <br />
+              <span>特徴: </span>1度あったら忘れない <br />
+              <span>使用Mac: </span>2.２GHz6 Intel Core i7 <br />
+              <span>使用ソフト: </span> <br />
+              CLIP STUDIO RAINT, Adobe Illustrator 2020, <br />
               Photoshop 2020
-
             </p>
             <p class="profile-l-txt2">
-              <span>経歴:</span> <br>
-              パステルという画材に魅了され日本福祉パステルアート協会にて一級を取得。パステル教室を開催しつつイラスト販売などアーティスト活動をする。 <br>
+              <span>経歴:</span> <br />
+              パステルという画材に魅了され日本福祉パステルアート協会にて一級を取得。パステル教室を開催しつつイラスト販売などアーティスト活動をする。
+              <br />
               2020年絵本学校「ウーマンクリエイターズカレッジ」に入学。卒業後イラストレーターつぼいひろき氏の元で多くを学ぶ。
               現在は同学校の所属イラストレーターとなる。また、身近な人たちの辛い不妊治療経験を聞き子宝祈願の応援を兼ねた活動を開始。
               そこで、子授け名門神社にて念願の「子授け絵馬」の販売を始める。
@@ -41,7 +48,7 @@
             </p>
           </div>
           <div class="profile-r">
-            <img src="/images/img1.jpg" width="500" height="650" alt="" loading="lazy">
+            <img src="/images/img1.jpg" width="500" height="650" alt="" loading="lazy" />
           </div>
         </div>
       </section>
@@ -51,23 +58,29 @@
         <div class="flex">
           <div class="box">
             <div class="works-img">
-              <img src="/images/img2.jpg" width="300" height="210" alt="" loading="lazy">
+              <img src="/images/img2.jpg" width="300" height="210" alt="" loading="lazy" />
             </div>
             <h3 class="works-text">イラスト集</h3>
-            <p class="works-text2">まるで絵本の 1 ページのようなストーリー性のあるイラストが得意です 楽しく遊ぶ子供達や夢のある雰囲気満載のイラストをお描きします。</p>
+            <p class="works-text2">
+              まるで絵本の 1 ページのようなストーリー性のあるイラストが得意です
+              楽しく遊ぶ子供達や夢のある雰囲気満載のイラストをお描きします。
+            </p>
             <a href="/oder/" class="art-btn">作品一覧はこちら</a>
           </div>
           <div class="box">
             <div class="works-img">
-              <img src="/images/img3.jpg" width="300" height="210" alt="" loading="lazy">
+              <img src="/images/img3.jpg" width="300" height="210" alt="" loading="lazy" />
             </div>
             <h3 class="works-text">挿し絵製作</h3>
-            <p class="works-text2">赤ちゃんから子供のイラストの挿絵等、なんでも書き上げます。 年齢別に可愛らしくかき分けます。タッチや顔のご希望があれば納得いくまで描きます。</p>
+            <p class="works-text2">
+              赤ちゃんから子供のイラストの挿絵等、なんでも書き上げます。
+              年齢別に可愛らしくかき分けます。タッチや顔のご希望があれば納得いくまで描きます。
+            </p>
             <a href="/artwork/" class="art-btn">作品一覧はこちら</a>
           </div>
           <div class="box">
             <div class="works-img">
-              <img src="/images/img5.png" width="300" height="210" alt="" loading="lazy">
+              <img src="/images/img5.png" width="300" height="210" alt="" loading="lazy" />
             </div>
             <h3 class="works-text">子授け祈願</h3>
             <p class="works-text2">
@@ -77,37 +90,35 @@
           </div>
           <div class="box">
             <div class="works-img">
-              <img src="/images/img4.jpg" width="300" height="210" alt="" loading="lazy">
+              <img src="/images/img4.jpg" width="300" height="210" alt="" loading="lazy" />
             </div>
             <h3 class="works-text">絵馬イラスト</h3>
             <p class="works-text2">
-              神社様の完全なオリジナル絵馬を作成しませんか？ <br>
-              神社様名や言葉に加え個性的で目を引くイラストを提案いたします。 子授けを始め健康祈願・学問、縁結び等どんな絵馬もお描きします。
+              神社様の完全なオリジナル絵馬を作成しませんか？ <br />
+              神社様名や言葉に加え個性的で目を引くイラストを提案いたします。
+              子授けを始め健康祈願・学問、縁結び等どんな絵馬もお描きします。
               絵馬のサイズも多種対応可能です。是非一度ご相談下さい。
             </p>
             <a href="/votive/" class="art-btn">作品一覧はこちら</a>
           </div>
           <div class="box">
             <div class="works-img">
-              <img src="/images/img7.jpg" width="300" height="210" alt="" loading="lazy">
+              <img src="/images/img7.jpg" width="300" height="210" alt="" loading="lazy" />
             </div>
             <h3 class="works-text">似顔絵イラスト</h3>
             <p class="works-text2">
               人物だけでなく猫ちゃんやわんちゃんなど大切なご家族をイラストにします。
-              パステルタッチが得意ですが水彩などご要望にお応え致します。
-              PDFデータと印刷したものをお渡しします<br>
-
+              パステルタッチが得意ですが水彩などご要望にお応え致します。 PDFデータと印刷したものをお渡しします<br />
             </p>
             <a href="/portrait/" class="art-btn">作品一覧はこちら</a>
           </div>
           <div class="box">
             <div class="works-img">
-              <img src="/images/img6.jpeg" width="300" height="210" alt="" loading="lazy">
+              <img src="/images/img6.jpeg" width="300" height="210" alt="" loading="lazy" />
             </div>
             <h3 class="works-text">カレンダーイラスト</h3>
             <p class="works-text2">
-              可愛らしいキャラクターや筆者得意の天使・子授けなど四季に合わせた
-              カレンダーらしいイラストを作成致します。
+              可愛らしいキャラクターや筆者得意の天使・子授けなど四季に合わせた カレンダーらしいイラストを作成致します。
               めくるたびにワクワクドキドキするような楽しいカレンダーイラストを提案いたします。
             </p>
             <a href="/calendar/" class="art-btn">作品一覧はこちら</a>
@@ -121,22 +132,21 @@
           <tr>
             <th>2013年</th>
             <td>山田養蜂場チャリティカレンダーイラスト提供</td>
-
           </tr>
           <tr>
             <th>2014年</th>
             <td>
-              minne ミンネにて作品販売ギャラリー開設 <br>
+              minne ミンネにて作品販売ギャラリー開設 <br />
               (<a href="https://minne.com/@yukoppi" target="_blank">https://minne.com/@yukoppi</a>)
             </td>
-
           </tr>
           <tr>
             <th>2015年</th>
             <td>
-              Creema クリーマにて作品販売ギャラリー開設 <br>
-              (<a href="https://www.creema.jp/creator/66701/item/onsale"
-                target="_blank">https://www.creema.jp/creator/66701/item/onsale</a>)
+              Creema クリーマにて作品販売ギャラリー開設 <br />
+              (<a href="https://www.creema.jp/creator/66701/item/onsale" target="_blank"
+                >https://www.creema.jp/creator/66701/item/onsale</a
+              >)
             </td>
           </tr>
           <tr>
@@ -146,26 +156,26 @@
           <tr>
             <th>2015年9月</th>
             <td>
-              参道ikibaniにてワークショップフェスティバル TU・KU・RUに参加。<br>
+              参道ikibaniにてワークショップフェスティバル TU・KU・RUに参加。<br />
               パステル画の原画販売と塗り絵教室を行う。
-
             </td>
           </tr>
           <tr>
             <th>2019年3月</th>
             <td>
-              メルカリに出品開始 <br>
-              (<a href="https://www.mercari.com/jp/u/151914003/"
-                target="_blank">https://www.mercari.com/jp/u/151914003/</a>)
+              メルカリに出品開始 <br />
+              (<a href="https://www.mercari.com/jp/u/151914003/" target="_blank"
+                >https://www.mercari.com/jp/u/151914003/</a
+              >)
             </td>
-
           </tr>
           <tr>
             <th>2019年8月</th>
             <td>
-              日本福祉パステルアート協会にインストラクターとしてインタビューを受ける <br>
-              (<a href="https://fukushi-pastelart.com/ikunoublog/instructor/11578/"
-                target="_blank">https://fukushi-pastelart.com/ikunoublog/instructor/11578/</a>)
+              日本福祉パステルアート協会にインストラクターとしてインタビューを受ける <br />
+              (<a href="https://fukushi-pastelart.com/ikunoublog/instructor/11578/" target="_blank"
+                >https://fukushi-pastelart.com/ikunoublog/instructor/11578/</a
+              >)
             </td>
           </tr>
           <tr>
@@ -183,41 +193,42 @@
           <tr>
             <th>2020年12月</th>
             <td>
-              東京新大久保の夫婦木神社様にて子授け絵馬を販売開始。
-              (<a href="http://meotogi.tokyo/">http://meotogi.tokyo/</a>)
+              東京新大久保の夫婦木神社様にて子授け絵馬を販売開始。 (<a href="http://meotogi.tokyo/"
+                >http://meotogi.tokyo/</a
+              >)
             </td>
           </tr>
           <tr>
             <th>2021年9月</th>
             <td>
-              ウーマンクリエイターズカレッジにて絵本展開催決定絵本「三日ぼうずのみっかさん」出展<br>
-              (<a
-                href="https://woman-college.com/blog/exhibition_20210423/">https://woman-college.com/blog/exhibition_20210423/</a>)
+              ウーマンクリエイターズカレッジにて絵本展開催決定絵本「三日ぼうずのみっかさん」出展<br />
+              (<a href="https://woman-college.com/blog/exhibition_20210423/"
+                >https://woman-college.com/blog/exhibition_20210423/</a
+              >)
             </td>
           </tr>
           <tr>
             <th>2021年5月</th>
             <td>
-              LINEスタンプ販売中。是非使ってみてください！<br>
-              (<a
-                href="https://store.line.me/stickershop/product/8428074/ja">https://store.line.me/stickershop/product/8428074/ja</a>)
+              LINEスタンプ販売中。是非使ってみてください！<br />
+              (<a href="https://store.line.me/stickershop/product/8428074/ja"
+                >https://store.line.me/stickershop/product/8428074/ja</a
+              >)
             </td>
           </tr>
-           <tr>
+          <tr>
             <th>2021年9月</th>
             <td>
-              「第13回ねこまつりat湯島」限定ねこグッズを多数販売します！是非遊びに来てください<br>
-              (<a
-                href="https://nekomatsuri.com/">https://nekomatsuri.com/</a>)
+              「第13回ねこまつりat湯島」限定ねこグッズを多数販売します！是非遊びに来てください<br />
+              (<a href="https://nekomatsuri.com/">https://nekomatsuri.com/</a>)
             </td>
           </tr>
         </table>
-
       </section>
 
       <section class="contact">
         <h2>お問い合わせ</h2>
-        <p>お仕事のご依頼・ご相談は下記のメールアドレスにお願いします。<br></p>
+        <p>お仕事のご依頼・ご相談は下記のメールアドレスにお願いします。<br /></p>
         <table>
           <tr>
             <th>E-mail:</th>
@@ -226,7 +237,6 @@
             </td>
           </tr>
         </table>
-
       </section>
     </main>
     <footer class="footer">
@@ -251,64 +261,57 @@
 export default {
   head() {
     return {
-      title:
-        "ささら優 ポートフォリオサイト",
-      titleTemplate: "",
+      title: 'ささら優 ポートフォリオサイト',
+      titleTemplate: '',
       meta: [
-        { name: "viewport", content: "width=device-width, initial-scale=1" },
+        { name: 'viewport', content: 'width=device-width, initial-scale=1' },
         {
-          hid: "description",
-          name: "description",
+          hid: 'description',
+          name: 'description',
           content:
-            "ささら優(ゆう)のポートフォリオサイトです。パステル教室を開催しつつイラスト販売などアーティスト活動をしています。"
+            'ささら優(ゆう)のポートフォリオサイトです。パステル教室を開催しつつイラスト販売などアーティスト活動をしています。'
         },
         {
-          hid: "keywords",
-          name: "keywords",
+          hid: 'keywords',
+          name: 'keywords',
+          content: 'ささらゆう, ささら優, パステル画, パステル, イラストレーター, イラスト, 絵'
+        },
+        {
+          hid: 'og:image',
+          name: 'og:image',
+          content: '/images/mv.png'
+        },
+        {
+          hid: 'twitter:image',
+          name: 'twitter:image',
+          content: '/images/fv.jpg'
+        },
+        {
+          hid: 'og:title',
+          name: 'og:title',
+          content: 'ささら優 ポートフォリオサイト'
+        },
+        {
+          hid: 'twitter:title',
+          name: 'twitter:title',
+          content: 'ささら優 ポートフォリオサイト'
+        },
+        {
+          hid: 'og:description',
+          name: 'og:description',
           content:
-            "ささらゆう, ささら優, パステル画, パステル, イラストレーター, イラスト, 絵"
+            'ささら優(ゆう)のポートフォリオサイトです。パステル教室を開催しつつイラスト販売などアーティスト活動をしています。'
         },
         {
-          hid: "og:image",
-          name: "og:image",
-          content: "/images/mv.png"
-        },
-        {
-          hid: "twitter:image",
-          name: "twitter:image",
-          content: "/images/fv.jpg"
-        },
-        {
-          hid: "og:title",
-          name: "og:title",
+          hid: 'twitter:description',
+          name: 'twitter:description',
           content:
-            "ささら優 ポートフォリオサイト"
-        },
-        {
-          hid: "twitter:title",
-          name: "twitter:title",
-          content:
-            "ささら優 ポートフォリオサイト"
-        },
-        {
-          hid: "og:description",
-          name: "og:description",
-          content:
-            "ささら優(ゆう)のポートフォリオサイトです。パステル教室を開催しつつイラスト販売などアーティスト活動をしています。"
-        },
-        {
-          hid: "twitter:description",
-          name: "twitter:description",
-          content:
-            "ささら優(ゆう)のポートフォリオサイトです。パステル教室を開催しつつイラスト販売などアーティスト活動をしています。"
+            'ささら優(ゆう)のポートフォリオサイトです。パステル教室を開催しつつイラスト販売などアーティスト活動をしています。'
         }
       ]
-    };
-  },
+    }
+  }
 }
 </script>
 
-<style>
-
-
-</style>
+<style></style>
